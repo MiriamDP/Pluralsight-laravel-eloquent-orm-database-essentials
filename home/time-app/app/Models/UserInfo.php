fdeacae979fe74f1fc0854870e6094c148cafbd3
MiriamDP <miriam101d@gmail.com>
2026-05-22 09:37:53 +0200

    relacion 1 a 1

--- home/time-app/app/Models/UserInfo.php
+++ home/time-app/app/Models/UserInfo.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserInfo extends Model
 {
-    //
+    protected $fillable=[
+        'user_id',
+        'admin,'
+    ];
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
